f925787d5b37bb8ad4512888fffb44037d339eb3
Andrew Burr <me@dragovorn.com>
2021-08-29 19:42:09 -0400

    implement araragi annotation discoverer

--- src/main/java/tv/isshoni/winry/internal/annotation/manage/WinryAnnotationManager.java
+++ src/main/java/tv/isshoni/winry/internal/annotation/manage/WinryAnnotationManager.java
@@ -1,13 +1,11 @@
 package tv.isshoni.winry.internal.annotation.manage;
 
-import tv.isshoni.araragi.annotation.AttachTo;
-import tv.isshoni.araragi.annotation.Processor;
+import tv.isshoni.araragi.annotation.discovery.SimpleAnnotationDiscoverer;
 import tv.isshoni.araragi.annotation.internal.AnnotationManager;
+import tv.isshoni.araragi.annotation.model.IAnnotationDiscoverer;
 import tv.isshoni.araragi.annotation.model.IAnnotationProcessor;
-import tv.isshoni.araragi.annotation.model.IParameterSupplier;
 import tv.isshoni.araragi.annotation.model.IPreparedAnnotationProcessor;
 import tv.isshoni.araragi.logging.AraragiLogger;
-import tv.isshoni.araragi.stream.Streams;
 import tv.isshoni.winry.annotation.Bootstrap;
 import tv.isshoni.winry.entity.annotation.IWinryAnnotationManager;
 import tv.isshoni.winry.entity.annotation.IWinryAnnotationProcessor;
@@ -17,17 +15,9 @@ import tv.isshoni.winry.entity.bootstrap.IBootstrapper;
 import tv.isshoni.winry.entity.logging.ILoggerFactory;
 
 import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import org.reflections8.Reflections;
-import org.reflections8.scanners.SubTypesScanner;
-import org.reflections8.scanners.TypeAnnotationsScanner;
-import org.reflections8.util.ConfigurationBuilder;
 
 public class WinryAnnotationManager extends AnnotationManager implements IWinryAnnotationManager {
 
@@ -50,44 +40,19 @@ public class WinryAnnotationManager extends AnnotationManager implements IWinryA
     public void initialize(Bootstrap bootstrap) {
         LOGGER.debug("Initializing...");
 
-        ArrayList<String> packages = new ArrayList<>(Arrays.asList(bootstrap.loadPackage()));
-        packages.add("tv.isshoni.winry.annotation");
-
         LOGGER.debug("Performing annotation discovery...");
+        IAnnotationDiscoverer discoverer = new SimpleAnnotationDiscoverer(this);
+        discoverer.withPackages("tv.isshoni.winry.annotation");
+        discoverer.withPackages(bootstrap.loadPackage());
 
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .addScanners(new TypeAnnotationsScanner(), new SubTypesScanner(false))
-                .forPackages(packages.toArray(new String[0])));
-
-        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Processor.class);
-
-        // TODO: Maybe move some of these into specific methods for the annotation manager? Either that or
-        // TODO: create a discovery-related object to contain these methods + configurations. Either way, this
-        // TODO: feels like it should be included in the default annotation manager and not be winry-specific.
         LOGGER.debug("Loading parameter supplier annotations...");
-        classes.stream()
-                .map(c -> (Class<? extends Annotation>) c)
-                .filter(c -> c.isAnnotationPresent(Processor.class))
-                .filter(c -> Streams.to(c.getAnnotation(Processor.class).value())
-                        .anyMatch(IParameterSupplier.class::isAssignableFrom))
-                .forEach(this::discoverAnnotation);
+        discoverer.discoverParameterAnnotations();
 
         LOGGER.debug("Loading all other annotations...");
-        classes.stream()
-                .map(c -> (Class<? extends Annotation>) c)
-                .filter(c -> c.isAnnotationPresent(Processor.class))
-                .filter(c -> Streams.to(c.getAnnotation(Processor.class).value())
-                        .noneMatch(IParameterSupplier.class::isAssignableFrom))
-                .forEach(this::discoverAnnotation);
-
-        classes = reflections.getTypesAnnotatedWith(AttachTo.class);
+        discoverer.discoverAnnotations();
 
         LOGGER.debug("Attaching requested processors...");
-        classes.stream()
-                .filter(c -> c.isAnnotationPresent(AttachTo.class))
-                .filter(IAnnotationProcessor.class::isAssignableFrom)
-                .map(c -> (Class<IWinryAnnotationProcessor<Annotation>>) c)
-                .forEach(this::discoverProcessor);
+        discoverer.discoverAttachedProcessors();
 
         LOGGER.debug("Discovered " + getTotalProcessors() + " annotation processors.");
     }
